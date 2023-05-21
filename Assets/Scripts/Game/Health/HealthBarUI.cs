e5552c3b8fc23575603df4e5a5701d4749521264
wsa138 <wsaddesso@gmail.com>
2023-05-21 11:59:45 -0400

    Add Health Bar Interface

---
+++ Assets/Scripts/Game/Health/HealthBarUI.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarUI : MonoBehaviour
+{
+    [SerializeField] private UnityEngine.UI.Image _healthBarForegroundImage;
+
+    public void UpdateHealthBar(HealthController healthController)
+    {
+        _healthBarForegroundImage.fillAmount = healthController.RemainingHealthPercentage;
+    }
+}
--- Assets/Scripts/Game/Health/HealthController.cs
+++ Assets/Scripts/Game/Health/HealthController.cs
@@ -24,6 +24,8 @@ public class HealthController : MonoBehaviour
 
     public UnityEvent OnDamaged;
 
+    public UnityEvent OnHealthChanged;
+
     public void TakeDamage(float damageAmount)
     {
         if (_currentHealth == 0)
@@ -37,6 +39,8 @@ public class HealthController : MonoBehaviour
         }
 
         _currentHealth -= damageAmount;
+
+        OnHealthChanged.Invoke();
 
         if (_currentHealth < 0)
         {
@@ -61,6 +65,8 @@ public class HealthController : MonoBehaviour
         }
 
         _currentHealth += amountToAdd;
+
+        OnHealthChanged.Invoke();
 
         if (_currentHealth > _maximumHealth)
         {
