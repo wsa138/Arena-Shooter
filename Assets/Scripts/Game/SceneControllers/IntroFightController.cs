b2adef59b0c76cd573f1187732b4561f7cb46ed7
wsa138 <wsaddesso@gmail.com>
2023-05-29 17:01:56 -0400

    Added IntroScene Boss Entrance
    Created a new boss prefab. Reused Enemy ship scripts. Expanded IntroFightController script to instantiate the boss.

--- Assets/Scripts/Game/SceneControllers/IntroFightController.cs
+++ Assets/Scripts/Game/SceneControllers/IntroFightController.cs
@@ -7,11 +7,16 @@ public class IntroFightController : MonoBehaviour
 {
     // Name of next scene to load
     public string nextSceneName;
+    public GameObject bossPrefab;
+    public Transform bossSpawnPoint;
 
     // Function to call when the current scene ends
     public void EndScene()
     {
         Debug.Log("Loading Next Scene Started");
+
+        SpawnBoss();
+
         StartCoroutine(LoadNextSceneWithDelay());
     }
 
@@ -19,11 +24,17 @@ public class IntroFightController : MonoBehaviour
     private IEnumerator LoadNextSceneWithDelay()
     {
         // Wait 5 seconds
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(8f);
 
         // Load Scene
         SceneManager.LoadScene(nextSceneName);
         Debug.Log("Loading Next Scene Ended");
+    }
+
+    public void SpawnBoss()
+    {
+        // Instantiate the prefab at the current position with default rotation.
+        Instantiate(bossPrefab, bossSpawnPoint.position, bossSpawnPoint.rotation);
     }
 
 }
