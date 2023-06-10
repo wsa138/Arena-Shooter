43f20a775c1e85d1663466f7957057028f9aaa50
wsa138 <wsaddesso@gmail.com>
2023-06-10 13:39:53 -0400

    Fixed the way the Human faces.
    I had the sprite flipping only when mouse position x was greater than 0, but I needed to swap it when the mouse position was greater than the gameObject position.

--- Assets/Scripts/Game/Player/Human/HumanMovement.cs
+++ Assets/Scripts/Game/Player/Human/HumanMovement.cs
@@ -7,19 +7,26 @@ public class HumanMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
+    public Camera cam;
+    bool facingLeft = true;
+    private Transform armTransform;
+    private float currentPositionX;
 
     Vector2 movement;
+    Vector3 mousePos;
 
     // Trigger movement with Update
     private void Update()
     {
         GetInput();
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     // Actually move player with Fixed Update
     private void FixedUpdate()
     {
         Move();
+        FlipSprite();
     }
 
     private void GetInput()
@@ -32,5 +39,23 @@ public class HumanMovement : MonoBehaviour
     {
         // Moves player up, down, left right
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void FlipSprite()
+    {
+        // Get the gameOjbect x position
+        currentPositionX = transform.position.x;
+
+        if (mousePos.x < currentPositionX && facingLeft == false)
+        {
+            gameObject.transform.localScale = new Vector3(1, 1, 1);
+            facingLeft = true;
+        }
+
+        if (mousePos.x > currentPositionX && facingLeft == true)
+        {
+            gameObject.transform.localScale = new Vector3(-1, 1, 1);
+            facingLeft = false;
+        }
     }
 }
