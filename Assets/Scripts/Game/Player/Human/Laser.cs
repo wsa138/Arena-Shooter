4973e3d7f2f3abbb6c1730acfc2eab870100b4b8
wsa138 <wsaddesso@gmail.com>
2023-06-25 17:31:44 -0400

    Cleaned up tilemap. Added laser functionality.

---
+++ Assets/Scripts/Game/Player/Human/Laser.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<OnShipEnemyMovement>())
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+        else if (collision.GetComponent<CompositeCollider2D>())
+        {
+            Destroy(gameObject);
+        }
+    }
+}
