1c5123b7ea15d0889aab4f097b9b628a69d05354
wsa138 <wsaddesso@gmail.com>
2023-07-16 23:16:15 -0400

    Created script for PlayerSuperShip shooting.

---
+++ Assets/Scripts/Game/Player/SuperShip/ShootPlayerSuperShip.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ShootPlayerSuperShip : MonoBehaviour
+{
+    [SerializeField] GameObject projectilePrefab;
+    [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] float projectileLifetime = 5f;
+    [SerializeField] float firingRate = .2f;
+
+    private bool isFiring;
+
+    Coroutine firingCoroutine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Fire();
+    }
+
+    void Fire()
+    {
+        if (isFiring && firingCoroutine == null)
+        {
+            firingCoroutine = StartCoroutine(FireContinuously());
+        }       
+        else if(!isFiring && firingCoroutine != null)
+        {
+            StopCoroutine(firingCoroutine);
+            firingCoroutine = null;
+        }
+    }
+
+    IEnumerator FireContinuously()
+    {
+        while(true)
+        {
+            GameObject instance = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+
+            Rigidbody2D rb = instance.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                rb.velocity = transform.up * projectileSpeed;
+            }
+
+            Destroy(instance, projectileLifetime);
+            yield return new WaitForSeconds(firingRate);
+        }
+    }
+
+    void OnFire(InputValue value)
+    {
+        isFiring = value.isPressed;
+    }
+}
